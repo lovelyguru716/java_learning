a064a6eb4d73d175dcf2283c3d77196762ec5d35
lovelyguru-716 <guruchandran0228@gmail.com>
2025-04-27 19:21:56 +0530

    added my work

--- java/first.java
+++ java/first.java
@@ -3,5 +3,8 @@ package java_learning.java;
 class first {
     public static void main(String[] args) {
         System.out.println("Hello world"); 
+        System.out.println("My work is completed");
     }
+
+
 }
